c7f18a7a925e47659f4d9811c0188db57a09b588
Mark Nosal <mark.c.nosal@gmail.com>
2023-04-06 15:24:35 -0500

    finsihed code-along

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,33 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+function ItemForm({ addNewItem }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const itemData = {
+      'name': name,
+      'category': category,
+      'isInCart': false,
+    }
+    fetch('http://localhost:4000/items', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(itemData)
+    })
+      .then(response => response.json())
+      .then(data => addNewItem(data))
+      setName('')
+      setCategory('Produce')
+  }
+
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -7,9 +7,34 @@ function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    fetch('http://localhost:4000/items')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
+
+  function addNewItem(newItem) {
+    const updatedItems = [
+      ...items,
+      newItem,
+    ]
+    setItems(updatedItems)
+  }
+
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map(item => item.id !== updatedItem.id ? item : updatedItem)
+    setItems(updatedItems)
+  }
+
+
   function handleCategoryChange(category) {
     setSelectedCategory(category);
   }
+
+  function handleDelete(deletedItem) {
+    const updatedItems = items.filter(item => item.id === deletedItem.id ? false : true)
+    setItems(updatedItems)
+    }
 
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
@@ -19,14 +44,14 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm addNewItem={addNewItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} handleUpdateItem={handleUpdateItem} onDeleteItem={handleDelete} />
         ))}
       </ul>
     </div>
